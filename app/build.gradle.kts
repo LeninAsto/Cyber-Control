8e618d77fdaaefca98240a9922aab13d83e3aca3
Lenin Asto <119011731+LeninAsto@users.noreply.github.com>
2026-04-02 08:36:25 -0500

    Change minSdk version from 32 to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.leninasto.cybercontrol"
-        minSdk = 32
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
